d0d060c2fce9434e7cffcec56c620c0625dcf142
Rafiul Sabbir <iamrafiul@gmail.com>
2016-02-05 22:19:52 +0200

    Commit with a change in config/db.js

--- config/db.js
+++ config/db.js
@@ -1,4 +1,4 @@
 module.exports = {
-	//url : 'mongodb://localhost/stencil-dev'
-	url : 'mongodb://iamrafiul:ami420@apollo.modulusmongo.net:27017/eSemu3za'
+	//basic url string to connect mongoDB with NodeJS
+	url : 'mongodb://localhost/stencil-dev'
 };
